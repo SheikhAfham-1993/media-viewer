180698f918a8597eb4b9e55caa25cc3ccd15fa61
SheikhAfham-1993 <56600934+SheikhAfham-1993@users.noreply.github.com>
2023-11-02 18:51:21 +0100

    Added single page and singe media view route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import {RouterProvider, createBrowserRouter} from "react-router-dom"
 import SearchPage from "./pages/SearchPage";
+import SingleMediaView from "./pages/SingleMediaView";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <SearchPage/>
+  },
+  {
+    path: "/singlemediaview/:id",
+    element: <SingleMediaView/>
   }
 ])
 function App() {
---
+++ src/pages/SingleMediaView.jsx
@@ -0,0 +1,8 @@
+
+const SingleMediaView = () => {
+    return (
+        <div>This is a Single Media view</div>
+    )
+}
+
+export default SingleMediaView
