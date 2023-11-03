5b7d0ac3740025fd523a44a3712f9eb213c57abe
SheikhAfham-1993 <56600934+SheikhAfham-1993@users.noreply.github.com>
2023-11-03 20:24:26 +0100

    Renamed fetchMedia prop to fetchMediaData to avoid confusion

--- src/components/SearchMedia/SearchMedia.jsx
+++ src/components/SearchMedia/SearchMedia.jsx
@@ -5,10 +5,10 @@ import { useRef } from 'react'
 /**
  * Renders a search input field with an optional clear button.
  *
- * @param {object} fetchMedia - A function that fetches media based on a search query.
+ * @param {object} fetchMediaData - A function that fetches media based on a search query.
  * @return {JSX.Element} The rendered search input field.
  */
-const SearchMedia = ({ fetchMedia }) => {
+const SearchMedia = ({ fetchMediaData }) => {
   const searchInputRef = useRef(null)
   return (
     <div className="search-container">
@@ -19,7 +19,7 @@ const SearchMedia = ({ fetchMedia }) => {
           type="text"
           placeholder="Search.."
           onKeyDown={(e) => {
-            if (e.key === 'Enter') fetchMedia(e.target.value)
+            if (e.key === 'Enter') fetchMediaData(e.target.value)
           }}
         />
         <XMarkIcon
