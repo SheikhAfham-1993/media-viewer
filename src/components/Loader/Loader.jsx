8e219a3421f8e4003be5ac81c60afac60815d2c6
SheikhAfham-1993 <56600934+SheikhAfham-1993@users.noreply.github.com>
2023-11-03 14:48:22 +0100

    Added loader to show the state (Loading or not)

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import './Loader.css'
+const Loader = () => {
+  return (
+    <div className="lds-container">
+      <div class="lds-ring">
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
