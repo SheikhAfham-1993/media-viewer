b5f68be062b5aac81c3522e36515a0bb5d80876f
SheikhAfham-1993 <56600934+SheikhAfham-1993@users.noreply.github.com>
2023-11-03 14:47:53 +0100

    Added Mock API to fetch media

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,71 +1,63 @@
 import './SearchPage.css'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import Loader from '../../components/Loader/Loader'
+
 const SearchPage = () => {
-  const mockData = {
-    'took-ms': 352,
-    amount: 6,
-    media: [
-      {
-        'media-id': 115446019,
-        preview: '/bild/st/0115446019/w.jpg',
-        height: 5180,
-        width: 7695,
-      },
-      {
-        'media-id': 138384880,
-        preview: '/bild/st/0138384880/w.jpg',
-        height: 5568,
-        width: 3712,
-      },
-      {
-        'media-id': 137849760,
-        preview: '/bild/st/0137849760/w.jpg',
-        height: 3537,
-        width: 5315,
-      },
-      {
-        'media-id': 150105569,
-        preview: '/bild/st/0150105569/w.jpg',
-        height: 2667,
-        width: 4000,
-      },
-      {
-        'media-id': 135909679,
-        preview: '/bild/st/0135909679/w.jpg',
-        height: 3648,
-        width: 5472,
-      },
-      {
-        'media-id': 115278567,
-        preview: '/bild/st/0115278567/w.jpg',
-        height: 7360,
-        width: 4912,
-      },
-    ],
+  const [media, setMedia] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  const fetchMedia = async (query = '') => {
+    let url =
+      'https://1780bf9a-10a6-4235-8605-39539ff6a76b.mock.pstmn.io/search'
+    if (query) {
+      url = `https://1780bf9a-10a6-4235-8605-39539ff6a76b.mock.pstmn.io/search?query=${query}`
+    }
+    setIsLoading(true)
+    const response = await axios.get(url)
+    setMedia(response.data.media)
+    setIsLoading(false)
   }
+
+  useEffect(() => {
+    fetchMedia()
+  }, [])
+
   return (
     <div className="container">
       <div className="search-container">
         <div className="input-container">
-          <input className="search-input" type="text" placeholder="Search.." />
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Search.."
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') fetchMedia(e.target.value)
+            }}
+          />
           <XMarkIcon className="clear-input" />
         </div>
       </div>
-      <div className="media-container media-content">
-        {mockData.media.map((media) => (
-          <Link
-            key={media['media-id']}
-            to={`/singlemediaview/${media['media-id']}`}
-          >
-            <img
-              className="media img"
-              alt="imago-media"
-              src={`https://www.imago-images.com/${media.preview}`}
-            />
-          </Link>
-        ))}
-      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="media-container media-content">
+          {media?.map((media) => (
+            <Link
+              key={media['media-id']}
+              to={`/singlemediaview/${media['media-id']}`}
+            >
+              <img
+                className="media img"
+                alt="imago-media"
+                src={`https://www.imago-images.com/${media.preview}`}
+              />
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
