24f6216944186b13d8e5a36f2810df382ec9b713
SheikhAfham-1993 <56600934+SheikhAfham-1993@users.noreply.github.com>
2023-11-05 19:16:52 +0100

    Added comment about why baseURL is in the app as a variable for this project

--- src/apis/fetchMedia.js
+++ src/apis/fetchMedia.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 // base url for mock api
+// This should be in the .env file but for demo purposes I am using it here.
+
 const baseURL = 'https://1780bf9a-10a6-4235-8605-39539ff6a76b.mock.pstmn.io/'
 
 /**
