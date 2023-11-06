ffd0f9e6b6c211a3abc19786d2ec87c7dfaf526d
SheikhAfham-1993 <56600934+SheikhAfham-1993@users.noreply.github.com>
2023-11-06 07:06:14 +0100

    Added comments regarding where browserRoutes is coming from

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { RouterProvider } from 'react-router-dom'
 import { browserRoutes } from './routes/routes'
 
-const router = browserRoutes
+const router = browserRoutes // All the routes for the application
 
 function App() {
+  // Initializing with the Router Provider to manage routing.
   return <RouterProvider router={router} />
 }
 
